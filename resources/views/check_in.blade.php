1bed5bc7c5438c92fbd001bcacd91580709cedee
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2025-10-23 23:32:26 +0900

    文言修正

--- resources/views/check_in.blade.php
+++ resources/views/check_in.blade.php
@@ -11,7 +11,8 @@
             <div class="event-title">
                 ETRO per Kaito Takahashi for holiday<br>
                 11月26日〜28日販売イベント<br>
-                入場抽選応募フォーム
+                入場抽選応募フォーム<br>
+                <span style="font-size:1rem;font-weight: 400;">11/29（土）以降は一般販売とします。但し当日の混雑状況に応じ整理券を配布する場合があります</span>
             </div>
             <div class="content-area">有効なチケットです</div>
             <div class="content-area">
