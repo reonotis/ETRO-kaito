7d52af9bf75c04404b6f5fbaad429ccf83de874b
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2026-07-06 11:58:34 +0900

    文言修正

--- resources/views/hankyu/hero.blade.php
+++ resources/views/hankyu/hero.blade.php
@@ -3,5 +3,5 @@
     <img src="{{ asset('image/LOGO.png') }}" alt="ETRO logo" class="event-logo">
     「ETRO per Kaito Takahashi」<br>
     入場抽選エントリー<br>
-    8月5日～9日 阪急うめだ本店<br>
+    <span class="event-subtitle">8月5日～9日 阪急うめだ本店1階 コトコトステージ11</span>
 </div>
