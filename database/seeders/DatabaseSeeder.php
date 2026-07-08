4059afb1b6c68456019cd279e30866558c251e17
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2026-07-08 22:50:43 +0900

    シーダー修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
                 'password' => 'password',
             ], [
                 'name' => '谷畑',
-                'email' => env('MAIL_SECRETARIAT'),
+                'email' => 'yabata@king-beat.com',
                 'password' => 'yabata#abc123',
 //            ], [
 //                'name' => '大村 沙織',
